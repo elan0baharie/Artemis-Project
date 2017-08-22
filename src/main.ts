3d4fb7f5033d76960f73242cd5fc8a59a9dfed0d
Aaron Nguyen <aaron3831@gmail.com>
2017-08-22 15:31:56 -0700

    add email sending script files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { AppComponent } from './app/app.component';
+import { UserService } from './app/users.service';
+import { HTTP_PROVIDERS } from '@angular/http';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
+
 
 platformBrowserDynamic().bootstrapModule(AppModule);
